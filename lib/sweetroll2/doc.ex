93b1f526d2a4689bdc8b3a979a0ac2ebcf744dfc
Greg V <greg@unrelenting.technology>
2019-03-28 14:45:05 +0300

    fmt import ecto, Doc.to_map

--- lib/sweetroll2/doc.ex
+++ lib/sweetroll2/doc.ex
@@ -13,7 +13,7 @@ defmodule Sweetroll2.Doc do
     field :children, {:array, :map}
   end
 
-  @real_fields [:url, :type, :deleted, :place_id, :entree_id]
+  @real_fields [:url, :type, :deleted, :published, :acl, :children]
 
   def changeset(struct, params) do
     {allowed, others} = Map.split(params, @real_fields)
@@ -21,5 +21,21 @@ defmodule Sweetroll2.Doc do
 
     struct
     |> cast(params, [:props | @real_fields])
+  end
+
+  def to_map(%Sweetroll2.Doc{
+        props: props,
+        type: type,
+        deleted: deleted,
+        published: published,
+        acl: acl,
+        children: children
+      }) do
+    props
+    |> Map.put(:type, type)
+    |> Map.put(:deleted, deleted)
+    |> Map.put(:published, published)
+    |> Map.put(:acl, acl)
+    |> Map.put(:children, children)
   end
 end
