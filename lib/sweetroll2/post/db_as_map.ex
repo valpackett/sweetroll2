5d794d0651b1a4c93631f39aab358d2b53ce17c1
Greg V <greg@unrelenting.technology>
2019-07-16 02:37:58 +0300

    Speed up db_as_map with process dictionary caching

--- lib/sweetroll2/post/db_as_map.ex
+++ lib/sweetroll2/post/db_as_map.ex
@@ -6,29 +6,39 @@ defmodule Sweetroll2.Post.DbAsMap do
   The idea is that you can either use a Map
   (for a preloaded local snapshot of the DB or for test data)
   or this blank struct (for live DB access).
+
+  Process dictionary caching is used because Mnesia access is
+  not actually as fast as process-local data.
+  So this is designed for web requests, not long-running processes.
   """
 
   defstruct []
 
   @impl Access
   def fetch(%__MODULE__{}, key) do
-    case :mnesia.dirty_read(Sweetroll2.Post, key) do
-      # Memento.Query.Data.load is too dynamic -> too slow
-      [{Sweetroll2.Post, url, deleted, published, updated, acl, type, props, children} | _] ->
-        {:ok,
-         %Sweetroll2.Post{
-           url: url,
-           deleted: deleted,
-           published: published,
-           updated: updated,
-           acl: acl,
-           type: type,
-           props: props,
-           children: children
-         }}
+    if result = Process.get(key) do
+      {:ok, result}
+    else
+      case :mnesia.dirty_read(Sweetroll2.Post, key) do
+        # Memento.Query.Data.load is too dynamic -> too slow
+        [{Sweetroll2.Post, url, deleted, published, updated, acl, type, props, children} | _] ->
+          post = %Sweetroll2.Post{
+            url: url,
+            deleted: deleted,
+            published: published,
+            updated: updated,
+            acl: acl,
+            type: type,
+            props: props,
+            children: children
+          }
 
-      _ ->
-        :error
+          Process.put(key, post)
+          {:ok, post}
+
+        _ ->
+          :error
+      end
     end
   end
 end
