aa820549115cbbfbce55ab69b3e376837a7e9183
Greg V <greg@unrelenting.technology>
2019-08-16 21:52:22 +0300

    fixup! Add export task, fix import task, use tzdata in config

--- lib/mix/sweetroll2.export.ex
+++ lib/mix/sweetroll2.export.ex
@@ -7,12 +7,13 @@ defmodule Mix.Tasks.Sweetroll2.Export do
   @doc false
   def run(_raw_args) do
     :ok = Memento.start()
-    Process.sleep(500) # XXX: why do we need to wait for mnesia to pick up the db??
+    # XXX: why do we need to wait for mnesia to pick up the db??
+    Process.sleep(500)
 
     posts = Memento.transaction!(fn -> Memento.Query.all(Sweetroll2.Post) end)
 
     for post <- posts do
-      post |> Sweetroll2.Post.to_map |> Jason.encode! |> IO.puts
+      post |> Sweetroll2.Post.to_map() |> Jason.encode!() |> IO.puts()
     end
   end
 end
