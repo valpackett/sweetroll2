571df92d4277d081827dfabecb75d284a567ad4e
Greg V <greg@unrelenting.technology>
2019-08-21 21:56:58 +0300

    Wait for DB in bootstrap too

--- lib/mix/sweetroll2.bootstrap.ex
+++ lib/mix/sweetroll2.bootstrap.ex
@@ -6,6 +6,10 @@ defmodule Mix.Tasks.Sweetroll2.Bootstrap do
   @impl Mix.Task
   @doc false
   def run(_) do
+    :ok = Memento.start()
+    # XXX: why do we need to wait for mnesia to pick up the db??
+    Process.sleep(500)
+
     Sweetroll2.Application.bootstrap!()
   end
 end
