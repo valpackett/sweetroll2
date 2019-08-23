65cadf41590edbc43d4a9f2e654c015d402d58ed
Greg V <greg@unrelenting.technology>
2019-08-23 17:33:46 +0300

    Improved fetch

--- lib/sweetroll2/job/fetch.ex
+++ lib/sweetroll2/job/fetch.ex
@@ -18,21 +18,24 @@ defmodule Sweetroll2.Job.Fetch do
     # TODO: check IP address ranges too.. or just ban IP addreses
     ret_if u.host == nil || u.host == "localhost", do: {:local_host, u.host}
 
-    # TODO handle 410 Gone
     resp = HttpClient.get!(url, headers: [{"accept", "text/html"}])
+
+    ret_if resp.status == 410, do: {:gone, resp}
+
     html = Floki.parse(resp.body)
 
-    if check_mention == nil ||
-         Enum.any?(Floki.find(html, "a"), &href_matches?(&1, check_mention)) do
-      mf =
-        Microformats2.parse(html, url)
-        |> Convert.find_mf_with_url(url)
-        |> Convert.simplify()
+    ret_if check_mention &&
+             not Enum.any?(Floki.find(html, "a"), &href_matches?(&1, check_mention)),
+           do: {:no_mention, check_mention}
 
-      {:ok, mf}
-    else
-      {:no_mention, check_mention}
-    end
+    mf =
+      Microformats2.parse(html, url)
+      |> Convert.find_mf_with_url(url)
+      |> Convert.simplify()
+
+    ret_if is_nil(mf), do: {:no_microformat, html}
+
+    {:ok, mf}
   end
 
   def perform(
@@ -43,30 +46,70 @@ defmodule Sweetroll2.Job.Fetch do
       ) do
     Timber.add_context(que: %{job_id: Logger.metadata()[:job_id]})
 
-    {:ok, mf} = fetch(url, check_mention: check_mention)
+    case fetch(url, check_mention: check_mention) do
+      {:ok, mf} ->
+        if author = Convert.as_one(mf["author"]) do
+          author_url =
+            cond do
+              is_map(author) -> Convert.as_one(author["url"])
+              is_binary(author) -> author
+              true -> nil
+            end
 
-    Memento.transaction!(fn ->
-      post = Post.from_map(mf)
+          if author_url and is_binary(author_url) and
+               String.starts_with?(author_url, "http") do
+            Que.add(__MODULE__,
+              url: author_url,
+              check_mention: nil,
+              save_mention: nil,
+              notify_update: notify_update
+            )
+          end
+        end
 
-      if post.url != url,
-        do:
-          Logger.warn("URL mismatch '#{post.url}' vs #{url}",
-            event: %{fetch_url_mismatch: %{post: post.url, requested: url}}
-          )
+        Memento.transaction!(fn ->
+          post = Post.from_map(mf)
 
-      Memento.Query.write(%{post | url: url})
+          if post.url != url,
+            do:
+              Logger.warn("URL mismatch '#{post.url}' vs #{url}",
+                event: %{fetch_url_mismatch: %{post: post.url, requested: url}}
+              )
 
-      if !is_nil(save_mention) do
-        post = Memento.Query.read(Post, save_mention)
+          Memento.Query.write(%{post | url: url})
 
-        props =
-          Map.update(post.props, "comment", [url], fn comm ->
-            if url in comm, do: comm, else: comm ++ [url]
-          end)
+          if !is_nil(save_mention) do
+            post = Memento.Query.read(Post, save_mention)
 
-        Memento.Query.write(%{post | props: props})
-      end
-    end)
+            props =
+              Map.update(post.props, "comment", [url], fn comm ->
+                if url in comm, do: comm, else: comm ++ [url]
+              end)
+
+            Memento.Query.write(%{post | props: props})
+          end
+        end)
+
+      {:gone, _} ->
+        Memento.transaction!(fn ->
+          post = Memento.Query.read(Post, url)
+
+          if post do
+            Memento.Query.write(%{post | deleted: true})
+          end
+
+          if !is_nil(save_mention) do
+            post = Memento.Query.read(Post, save_mention)
+
+            props =
+              Map.update(post.props, "comment", [], fn comm ->
+                Enum.filter(comm, &(&1 != url))
+              end)
+
+            Memento.Query.write(%{post | props: props})
+          end
+        end)
+    end
 
     Events.notify_urls_updated(notify_update)
   end
