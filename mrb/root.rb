c911f3b9a8dbb34ef04c9069c04ee096fbfee23b
Greg V <greg@unrelenting.technology>
2019-09-22 21:01:13 +0300

    Add workaround for app engine brotli bug

--- mrb/root.rb
+++ mrb/root.rb
@@ -11,7 +11,8 @@ lambda do |env|
   end
   if (env["HTTP_COOKIE"].nil? || !env["HTTP_COOKIE"].include?("wheeeee=C")) &&
       File.exist?(File.join(ENV["SR2_STATIC_GEN_OUT_DIR"] || "out/", env["PATH_INFO"], "index.html"))
-    return [307, { "x-reproxy-url" => "/__out__" + env["PATH_INFO"] }, []]
+    out = (!env["HTTP_USER_AGENT"].nil? && env["HTTP_USER_AGENT"].include?("AppEngine-Google")) ? "crapp_engine" : "out"
+    return [307, { "x-reproxy-url" => "/__#{out}__" + env["PATH_INFO"] }, []]
   end
   return [399, {}, []]
 end
